ec06fd0fcbb299f75a7fdd8650023a6d3db52004
Aidos Utegulov <utegulov@gmail.com>
2019-01-11 15:55:50 +0600

    Change Service name

--- FileTransferWindowsService/ProjectInstaller.Designer.cs
+++ FileTransferWindowsService/ProjectInstaller.Designer.cs
@@ -39,7 +39,7 @@
             // 
             // serviceInstaller1
             // 
-            this.serviceInstaller1.ServiceName = "MyFileTransferService";
+            this.serviceInstaller1.ServiceName = "StreamApiWinService";
             // 
             // ProjectInstaller
             // 
